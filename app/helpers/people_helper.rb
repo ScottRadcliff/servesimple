0200444bf7ef823a26b3e1719edb36ad92f9193a
Kevin Marsh <kevin.marsh@gmail.com>
2014-12-31 14:35:19 -0500

    Display role in color if specified for role

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,9 @@
 module PeopleHelper
+
+  def role_label(role)
+    style = role.color.present? ? "background-color: #{role.color}" : nil
+
+    content_tag :span, role.name, class: "label label-default", style: style
+  end
+
 end
