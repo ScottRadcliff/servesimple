2c643db0ad34e788e5d8105192efa8573f9e7500
Kevin Marsh <kevin.marsh@gmail.com>
2014-12-31 12:02:57 -0500

    Working, not yet requiring login though

---
+++ spec/features/login_feature_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'Login' do
+  let!(:account) { create(:account, subdomain: 'fft', name: 'Food for Thought') }
+  let!(:account_2) { create(:account, subdomain: 'idhc', name: 'Iglesia Dedios Hermandad Cristiana') }
+  let!(:role) { create(:role, name: 'Volunteer', account: account) }
+  let!(:person) { create(:person, name: 'Marsh, Kevin', account: account, roles: [role], password: 'testing123') }
+
+  it 'succeeds with correct password to correct domain' do
+    visit new_person_session_url(subdomain: account.subdomain)
+
+    fill_in 'Email', with: 'kevin.marsh@gmail.com'
+    fill_in 'Password', with: 'testing123'
+    click_button 'Log in'
+
+    expect(page).to have_content('Signed in successfully.')
+
+    click_link 'Sign out'
+    expect(page).to have_content('Signed out successfully.')
+  end
+
+  it 'fails with incorrect password to correct domain' do
+    visit new_person_session_url(subdomain: account.subdomain)
+
+    fill_in 'Email', with: 'kevin.marsh@gmail.com'
+    fill_in 'Password', with: 'testing1234'
+    click_button 'Log in'
+
+    expect(page).to have_content('Invalid email or password.')
+  end
+
+  it 'fails with correct password to incorrect domain' do
+    visit new_person_session_url(subdomain: account_2.subdomain)
+
+    fill_in 'Email', with: 'kevin.marsh@gmail.com'
+    fill_in 'Password', with: 'testing123'
+    click_button 'Log in'
+
+    expect(page).to have_content('Invalid email or password.')
+  end
+end
